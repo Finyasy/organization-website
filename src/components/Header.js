02d74fb61c17203b64e2364e179867dd96ded5dc
Bryan Bosire <bryanbosire@gmail.com>
2023-01-22 18:25:14 +0300

    added classNames to the links and also header-title to Basel

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <div className="header">
-      <Link to="/">Homepage</Link>
-      <Link to="/about">About Us</Link>
-      <Link to="/products">Products and Services</Link>
-      <Link to="/contact">Contact</Link>
-    </div>
-  );
-}
-
-export default Header;
+    return (
+      <div className="header">
+        <div className="header-title">Basel Retirement Home and Care Center</div>
+        <div className="header-links">
+          <Link className="header-link" to="/">Homepage</Link>
+          <Link className="header-link" to="/about">About Us</Link>
+          <Link className="header-link" to="/products">Products and Services</Link>
+          <Link className="header-link" to="/contact">Contact</Link>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Header;
