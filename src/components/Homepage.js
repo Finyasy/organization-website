03fdca9ce94b4177581c08738f6210527c35383e
Bryan Bosire <bryanbosire@gmail.com>
2023-01-22 19:01:12 +0300

    Added contnet about the organization

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -12,8 +12,21 @@ const Homepage = () => {
       height: '100%',
       position: 'absolute'
     }}>
-      <h1>Welcome to our organization!</h1>
-      <p>Here is some information about us...</p>
+
+<div className="homepage-text-container">
+
+        <p>
+          Basel Retirement Home and Care Center is a nonprofit home and community care provider in the
+          northwestern city of Basel. We serve the inhabitants of the Basel-Stadt area. The Center’s staff,
+          volunteers, and partners are committed to supporting retirees and the elderly to lead independent
+          lives instead of relying on their families. The organization also offers this population care
+          services to enhance their quality of life. Presently, the retirement home provides a myriad of
+          living options. All our residences offer invaluable supportive services, including community, at-
+          home, social work, and specialized care and services. We also offer the residents an inviting
+          community, nutritious meals, and housekeeping services. The care center provides 24/7
+          assistance to ensure the residents live comfortably as they reach the end of life phase.
+        </p>
+      </div>
     </div>
   );
 }
