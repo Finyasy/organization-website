d5275dd2fbd9a12128de87b0b36106148923f291
Bryan Bosire <bryanbosire@gmail.com>
2023-01-22 17:59:09 +0300

    Removed Switch and used Router as per latest react-router-dom

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
+import Homepage from './components/Homepage';
+import About from './components/About';
+import Products from './components/Products';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
