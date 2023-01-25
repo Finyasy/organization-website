fd42dcfd2cefdfe0e19e6a7bee6efee31ef3a22e
Bryan Bosire <bryanbosire@gmail.com>
2023-01-25 12:59:07 +0300

    Added logo image to the Header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo2 from '../images/logo2.png'
 
 const Header = () => {
     return (
       <div className="header">
-        <div className="header-title">Basel Retirement Home and Care Center</div>
+        <div className="header-title">
+          Basel Retirement Home and Care Center
+          <img src={logo2} alt="Logo" className="header-logo" />
+        </div>
         <div className="header-links">
           <Link className="header-link" to="/">Homepage</Link>
           <Link className="header-link" to="/about">About Us</Link>
